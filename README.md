c0c5b5266ea0248312787f2257abb0628464d128
ARHMC <72113183@continental.edu.pe>
2026-04-09 01:22:30 -0500

    feat: proyecto inicial evaluacion crediticia web

---
+++ README.md
@@ -0,0 +1,14 @@
+# Evaluación Crediticia Web
+
+Sistema web que permite evaluar solicitudes de crédito usando lógica basada en reglas.
+
+## Tecnologías
+- HTML
+- CSS
+- JavaScript
+- Git & GitHub
+
+## Integrantes
+- ASTUHUAMAN VASQUEZ, AKEMY MAYUMI
+- HUAMAN LEVANO, LUIS ALBERTO
+- HURTADO DE MENDOZA COILA, ANGELO RANDY
---
+++ src/app.js
@@ -0,0 +1,26 @@
+function evaluar() {
+    const edad = parseFloat(document.getElementById("edad").value);
+    const ingresos = parseFloat(document.getElementById("ingresos").value);
+    const monto = parseFloat(document.getElementById("monto").value);
+    const empleo = parseFloat(document.getElementById("empleo").value);
+    const historial = parseInt(document.getElementById("historial").value);
+
+    if (!edad || !ingresos || !monto || historial === undefined) {
+        document.getElementById("resultado").textContent = "Ingrese todos los datos correctamente";
+        return;
+    }
+
+    let probabilidad;
+
+    if (ingresos > 2000 && historial === 1 && monto < ingresos * 5) {
+        probabilidad = 0.8;
+    } else {
+        probabilidad = 0.3;
+    }
+
+    if (probabilidad >= 0.5) {
+        document.getElementById("resultado").textContent = "✅ Crédito Aprobado";
+    } else {
+        document.getElementById("resultado").textContent = "❌ Crédito Rechazado";
+    }
+}
